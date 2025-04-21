2a4277c6548760b494b2be219549e1e2d84c8f69
ToxEvo <dawsalowy@gmail.com>
2025-04-21 22:36:08 +0100

    Style(Console): Tidy CPU information output formatting

--- Program.cs
+++ Program.cs
@@ -3,12 +3,18 @@
 var cpuService = new CpuInfoService();
 var cpu = cpuService.GetCpuInfo();
 
-Console.WriteLine("CPU Information:");
-Console.WriteLine($"Name: {cpu.Name}");
-Console.WriteLine($"Cores: {cpu.CoreCount}");
-Console.WriteLine($"Threads: {cpu.ThreadCount}");
-Console.WriteLine($"Base Clock: {cpu.BaseClockGHz} GHz");
-Console.WriteLine($"Current Clock: {cpu.CurrentClockGHz} GHz");
-Console.WriteLine($"Max Clock: {cpu.MaxClockGHz} GHz");
-Console.WriteLine($"Manufacturer: {cpu.Manufacturer}");
-Console.WriteLine($"Architecture: {cpu.Architecture}");
+Console.WriteLine(new string('-', 40));
+Console.WriteLine("CPU INFORMATION");
+Console.WriteLine(new string('-', 40));
+
+Console.WriteLine($"Name           : {cpu.Name}");
+Console.WriteLine($"Manufacturer   : {cpu.Manufacturer}");
+Console.WriteLine($"Architecture   : {cpu.Architecture}");
+Console.WriteLine($"Cores          : {cpu.CoreCount}");
+Console.WriteLine($"Threads        : {cpu.ThreadCount}");
+Console.WriteLine($"Base Clock     : {cpu.BaseClockGHz:F2} GHz");
+Console.WriteLine($"Current Clock  : {cpu.CurrentClockGHz:F2} GHz");
+Console.WriteLine($"Max Clock      : {cpu.MaxClockGHz:F2} GHz");
+
+Console.WriteLine(new string('-', 40));
+
